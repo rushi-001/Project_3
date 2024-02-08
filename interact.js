315706e9766371f8d86c4b8ae5ec696ac1d53351
RUSHI PANCHAL <panchalrushi2182004@gmail.com>
2024-02-08 19:39:03 +0530

    add background and fix the issue
    completed

--- interact.js
+++ interact.js
@@ -7,32 +7,33 @@ var mouseY = 0;
 
 // to get the client mouse position
 document.addEventListener("mousemove", function (event) {
-  
-    // storing the mouse position
-    mouseX = event.clientX;
+  // storing the mouse position
+  mouseX = event.clientX;
+  mouseY = event.clientY;
 
-    // get the position of the rectangle relative to the viewport
-  let rect = rectangle.getBoundingClientRect();
-//   console.log("Top:", rect.top);
-// console.log("left:", rect.left);
-// console.log("right:", rect.right);
-// console.log("bottom:", rect.bottom);
+  // get the position of the rectangle relative to the viewport
+  let rect = rectangle.getBoundingClientRect(); // getBoundingClientRect() is an function which gives the position of top, bottom, left, right
+  // console.log("Top:", rect.top);
+  // console.log("left:", rect.left);
+  // console.log("right:", rect.right);
+  // console.log("bottom:", rect.bottom);
 
-    // check the courser is the rectangle
-    let inBetween = (courser, top, bottom) => {
-        return courser >= Math.min(top, bottom) && courser <= Math.max(top, bottom);
-    }
-    
-    // finding the mid value of the rectangle
-    let rectangleMidPosition = (rect.left+rect.right)/2;
+  // check the courser is the rectangle
+  let inRectangle = (top, bottom, left, right, cursorX, cursorY) => {
+    return (
+      cursorX >= left && cursorX <= right && cursorY >= top && cursorY <= bottom
+    ); // it's returns true and false
+  };
 
-  if (mouseX <= rectangleMidPosition && inBetween(mouseX, rect.top, rect.bottom)) {
-    rectangle.style.backgroundColor = 'red';
-  } else if (mouseX >= rectangleMidPosition && inBetween(mouseX, rect.top, rect.bottom)){
+  // finding the mid value of the rectangle
+  let rectangleMidPosition = (rect.left + rect.right) / 2;
+
+  // change color
+  if (mouseX <= rectangleMidPosition && inRectangle(rect.top, rect.bottom, rect.left, rect.right, mouseX, mouseY)) {
+    rectangle.style.backgroundColor = "red";
+  } else if (mouseX >= rectangleMidPosition && inRectangle(rect.top, rect.bottom, rect.left, rect.right, mouseX, mouseY)) {
     rectangle.style.backgroundColor = "blue";
   } else {
     rectangle.style.backgroundColor = "black";
   }
-
 });
-
